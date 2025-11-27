49f7d359c457e10fe216c801133abbb35a2ff90a
692873036 <692873036@qq.com>
2025-11-27 13:52:43 +0800

    更新 build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,77 +1,21 @@
-import com.android.build.api.dsl.ApplicationDefaultConfig
-import com.android.build.api.dsl.CommonExtension
-import com.android.build.gradle.api.AndroidBasePlugin
+@file:Suppress("UnstableApiUsage")
+
+import com.android.build.gradle.tasks.PackageAndroidArtifact
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.net.URI
 
 plugins {
-    alias(libs.plugins.agp.app) apply false
-    alias(libs.plugins.agp.lib) apply false
-    alias(libs.plugins.kotlin) apply false
-    alias(libs.plugins.kotlin.compose.compiler) apply false
+    alias(libs.plugins.agp.app)
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.kotlin.compose.compiler)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.lsplugin.apksign)
+    alias(libs.plugins.lsplugin.resopt)
+    alias(libs.plugins.lsplugin.cmaker)
+    id("kotlin-parcelize")
 }
 
-project.ext.set("kernelPatchVersion", "0.12.2")
-
-val androidMinSdkVersion = 26
-val androidTargetSdkVersion = 36
-val androidCompileSdkVersion = 36
-val androidBuildToolsVersion = "36.0.0"
-val androidCompileNdkVersion = "29.0.14206865"
-val managerVersionCode by extra(getVersionCode())
-val managerVersionName by extra(getVersionName())
-val branchname by extra(getbranch())
-fun Project.exec(command: String) = providers.exec {
-    commandLine(command.split(" "))
-}.standardOutput.asText.get().trim()
-
-fun getGitCommitCount(): Int {
-    return exec("git rev-list --count HEAD").trim().toInt()
-}
-
-fun getGitDescribe(): String {
-    return exec("git rev-parse --verify --short HEAD").trim()
-}
-
-fun getVersionCode(): Int {
-    val commitCount = getGitCommitCount()
-    val major = 1
-    return major * 10000 + commitCount + 200
-}
-
-fun getbranch(): String {
-    return exec("git rev-parse --abbrev-ref HEAD").trim()
-}
-
-fun getVersionName(): String {
-    return getGitDescribe()
-}
-
-tasks.register("printVersion") {
-    doLast {
-        println("Version code: $managerVersionCode")
-        println("Version name: $managerVersionName")
-    }
-}
-
-subprojects {
-    plugins.withType(AndroidBasePlugin::class.java) {
-        extensions.configure(CommonExtension::class.java) {
-            compileSdk = androidCompileSdkVersion
-            buildToolsVersion = androidBuildToolsVersion
-            ndkVersion = androidCompileNdkVersion
-
-            defaultConfig {
-                minSdk = androidMinSdkVersion
-                if (this is ApplicationDefaultConfig) {
-                    targetSdk = androidTargetSdkVersion
-                    versionCode = managerVersionCode
-                    versionName = managerVersionName
-                }
-            }
-
-            lint {
-                abortOnError = true
-                checkReleaseBuilds = false
-            }
-        }
-    }
-}
+val managerVersionCode: Int by rootProject.extra
+val managerVersionName: String by rootProject.extra
+val branchname: String by rootProject.extra
+val kernelPatchVersion: 
